5dd0a5313df9d0993413e8032162f2e10a7d6bc1
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-11 08:54:30 +0800

    test

--- index.js
+++ index.js
@@ -10,5 +10,17 @@ git.status((err,res)=>{
     git.add('.',(err,res)=>{
         if(err){console.error(err)};
         console.log(res);
+        git.commit('test',(err,res)=>{
+            if(err){console.error(err)};
+            console.log(res);
+            git.pullOrigin('main',(err,res)=>{
+                if(err){console.error(err)};
+                console.log(res);
+                git.pushOrigin('main',(err,res)=>{
+                    if(err){console.error(err)};
+                    console.log(res);
+                });
+            });
+        });
     });
 });
